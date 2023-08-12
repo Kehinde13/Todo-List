0c77a8f0b7c8425c69ac8b8b408647222b20fdbc
Kehinde Balogun <balogunkehinde3@gmail.com>
2023-08-11 17:01:59 -0700

    Added updates to the app js

--- js/app.js
+++ js/app.js
@@ -1,84 +1,131 @@
 const clear = document.getElementById("clear");
 const date = document.getElementById("date");
 const list = document.getElementById("list");
-const input = document.getElementById("input");
-const AddContent = document.getElementById("add-content");
+const input = document.querySelector("input");
+const addTodo = document.getElementById("add-content");
 const save = document.getElementById("saveBtn");
-const savedToDo = localStorage.getItem("To-Do");
+const addTodoForm = document.querySelector("#addTodoForm")
 
 
 
-//show today's date
+//get today's date
 const options = {weekday: "long", month:"short", day:"numeric"};
 const today = new Date();
 
+//show current date
 date.innerHTML = today.toLocaleDateString("en-Us", options);
 
- //save To-Do 
-const SaveToDo = () =>{
-localStorage.setItem("To-Do", list.innerHTML);
-console.log("saved");
-} 
 
-//display To-Do
-if(savedToDo){
-     list.innerHTML = savedToDo;  
+//create a new Todo
+const createTodo = (todo) => `
+     <li class="item">
+       <i class="fa fa-circle-thin co" onClick="toggleTodoCompletion(event)"></i>
+       <p class="text">
+          ${todo}
+       </p>
+       <i class="fa fa-trash-o de" onClick="deleteTodo(event)"></i>
+     </li>
+`
+
+//check for saved todos, if there is no todo set the savedTodos to an empty array
+savedTodos = JSON.parse(localStorage.getItem("todos")) || []
+
+//render all saved Todos 
+const renderTodos = () => {
+    savedTodos.forEach((todo) => {
+        list.insertAdjacentHTML(
+           'beforeend',
+           createTodo(todo)
+        )
+    })
 }
 
-//add To-Do
-AddContent.addEventListener('click', () =>{
-   
-     // if input is empty
-     if(input.value.length == ""){
+//display all the saved todos
+window.onload = renderTodos
+
+
+// add a new todo
+const addNewTodo = (event) => {
+     //prevent the form from submitting
+     event.preventDefault()
+
+     //check if the input is empty
+     if(input.value.length === ""){
           alert("Write a To-Do");
      } else {
-          //create Elements
-          const li = document.createElement('li');
-          const circle = document.createElement('i');
-          const paragraph = document.createElement('p');
-          const trash = document.createElement('i');
 
-          //add class list
-          li.classList.add("item");
-          circle.classList.add("fa","fa-circle-thin","co");
-          paragraph.classList.add("text");
-          trash.classList.add("fa","fa-trash-o","de");
-
-          //Append child
-          li.appendChild(circle);
-          li.appendChild(paragraph);
-          li.appendChild(trash);
-          list.appendChild(li);
-
-          //Add content
-          paragraph.innerText = input.value;
-      
-          //Clear input field
-          input.value = "";
-
-          //delete To-Do
-          trash.addEventListener('click', () => {
-               list.removeChild(li);
-          })
-
-          //cross out a To-Do
-          circle.addEventListener('click', () => {
-               circle.classList.remove("fa-circle-thin");
-               circle.classList.add("fa-check-circle");
-               paragraph.classList.add("lineThrough");
-          })
-
-          //save To-Do
-          save.addEventListener('click', SaveToDo);
-
+          //insert the new todo into the list of todos
+          const todo = input.value
+          const newTask = createTodo(todo)
+          list.insertAdjacentHTML(
+               'beforeend',
+               newTask
+          )
           
+          //save the new todo to localStorage
+          savedTodos.push(todo)
+          localStorage.setItem(
+               'todos',
+               JSON.stringify(savedTodos)
+          )
+          
+          //clear the input
+          addTodoForm.reset()
+     }
+    
+ }
 
-     }    
-})
+//add anew todo event
+ addTodo.addEventListener("click", addNewTodo)
 
+ //add a new todo when the enter key is pressed 
+ input.addEventListener('keydown', (event) => {
+     event.preventDefault()
+     if (event.key === 'Enter') {
+       addNewTodo;
+       console.log(addNewTodo);
+     }
+   });
+
+
+
+//check todo as completed
+const toggleTodoCompletion = (event) => {
+     //toggle the task completion
+     if(event.target.classList.contains("fa-circle-thin")) {
+          event.target.classList.remove("fa-circle-thin");
+          event.target.classList.add("fa-check-circle");
+          event.target.nextElementSibling.classList.add("lineThrough");
+     } else if (event.target.classList.contains("fa-check-circle")) {
+          event.target.classList.remove("fa-check-circle");
+          event.target.classList.add("fa-circle-thin");
+          event.target.nextElementSibling.classList.remove("lineThrough");
+     }
+     
+}
+
+//delete a todo
+const deleteTodo = (event) => {
+   //get the delete parentElement
+    const listItem = event.target.parentElement
+    //get the todo text and use it to access the index of the todo
+    const todo = listItem.querySelector(".text").innerText
+    const indexOfTask = savedTodos.indexOf(todo)
+    
+    //remove the todo from the saved todos nad the local storage
+    savedTodos.splice(indexOfTask, 1)
+    localStorage.setItem(
+        'todos',
+        JSON.stringify(savedTodos)
+    )
+    listItem.remove(todo)
+}
+
+//clear all todo
 clear.addEventListener('click', () => {
-     window.localStorage.clear('savedToDo');
-})
+     localStorage.clear('savedTodos');
+     list.remove()
+}) 
 
 
 
